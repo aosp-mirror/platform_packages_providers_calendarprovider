5bbabdd2f8732a844f28fe545dc2fbf97b2b47e9
Erik <roboerik@android.com>
2010-02-22 10:53:57 -0800

    b/2458172 Changed launch intent for widget.
    Changed the intent path for launching Calendar from com.android.calendar to com.google.android.calendar.
    This change reflects package name changes.

--- src/com/android/providers/calendar/CalendarAppWidgetService.java
+++ src/com/android/providers/calendar/CalendarAppWidgetService.java
@@ -46,7 +46,7 @@ import java.util.TimeZone;
 public class CalendarAppWidgetService extends Service implements Runnable {
     static final String TAG = "CalendarAppWidgetService";
     static final boolean LOGD = false;
-    
+
     static final String EVENT_SORT_ORDER = "startDay ASC, allDay DESC, begin ASC";
 
     static final String[] EVENT_PROJECTION = new String[] {
@@ -72,19 +72,19 @@ public class CalendarAppWidgetService extends Service implements Runnable {
     static final int INDEX_EVENT_LOCATION = 7;
     static final int INDEX_CALENDAR_ID = 8;
     static final int INDEX_EVENT_ID = 9;
-    
+
     static final long SEARCH_DURATION = DateUtils.WEEK_IN_MILLIS;
-    
+
     static final long UPDATE_NO_EVENTS = DateUtils.HOUR_IN_MILLIS * 6;
-    
-    static final String ACTION_PACKAGE = "com.android.calendar";
+
+    static final String ACTION_PACKAGE = "com.google.android.calendar";
     static final String ACTION_CLASS = "com.android.calendar.LaunchActivity";
     static final String KEY_DETAIL_VIEW = "DETAIL_VIEW";
-    
+
     @Override
     public void onStart(Intent intent, int startId) {
         super.onStart(intent, startId);
-        
+
         // Only start processing thread if not already running
         synchronized (AppWidgetShared.sLock) {
             if (!AppWidgetShared.sUpdateRunning) {
@@ -94,23 +94,23 @@ public class CalendarAppWidgetService extends Service implements Runnable {
             }
         }
     }
-    
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
-    
+
     /**
-     * Thread loop to handle 
+     * Thread loop to handle
      */
     public void run() {
         while (true) {
             long now = -1;
             int[] appWidgetIds;
             Set<Long> changedEventIds;
-            
+
             synchronized (AppWidgetShared.sLock) {
-                // Bail out if no remaining updates 
+                // Bail out if no remaining updates
                 if (!AppWidgetShared.sUpdateRequested) {
                     // Clear current shared state, release wakelock, and stop service
                     if (LOGD) Log.d(TAG, "no requested update or expired wakelock, bailing");
@@ -118,15 +118,15 @@ public class CalendarAppWidgetService extends Service implements Runnable {
                     stopSelf();
                     return;
                 }
-                
-                // Clear requested flag and collect latest parameters 
+
+                // Clear requested flag and collect latest parameters
                 AppWidgetShared.sUpdateRequested = false;
-                
+
                 now = AppWidgetShared.sLastRequest;
                 appWidgetIds = AppWidgetShared.collectAppWidgetIdsLocked();
                 changedEventIds = AppWidgetShared.collectChangedEventIdsLocked();
             }
-            
+
             // Process this update
             if (LOGD) Log.d(TAG, "processing requested update now=" + now);
             performUpdate(this, appWidgetIds, changedEventIds, now);
@@ -135,7 +135,7 @@ public class CalendarAppWidgetService extends Service implements Runnable {
 
     /**
      * Process and push out an update for the given appWidgetIds.
-     * 
+     *
      * @param context Context to use when updating widget.
      * @param appWidgetIds List of appWidgetIds to update, or null for all.
      * @param changedEventIds Specific events known to be changed, otherwise
@@ -145,7 +145,7 @@ public class CalendarAppWidgetService extends Service implements Runnable {
     private void performUpdate(Context context, int[] appWidgetIds,
             Set<Long> changedEventIds, long now) {
         ContentResolver resolver = context.getContentResolver();
-        
+
         Cursor cursor = null;
         RemoteViews views = null;
         long triggerTime = -1;
@@ -154,12 +154,12 @@ public class CalendarAppWidgetService extends Service implements Runnable {
             cursor = getUpcomingInstancesCursor(resolver, SEARCH_DURATION, now);
             if (cursor != null) {
                 MarkedEvents events = buildMarkedEvents(cursor, changedEventIds, now);
-                
+
                 boolean shouldUpdate = true;
                 if (changedEventIds.size() > 0) {
                     shouldUpdate = events.watchFound;
                 }
-                
+
                 if (events.primaryCount == 0) {
                     views = getAppWidgetNoEvents(context);
                 } else if (shouldUpdate) {
@@ -174,14 +174,14 @@ public class CalendarAppWidgetService extends Service implements Runnable {
                 cursor.close();
             }
         }
-        
+
         // Bail out early if no update built
         if (views == null) {
             if (LOGD) Log.d(TAG, "Didn't build update, possibly because changedEventIds=" +
                     changedEventIds.toString());
             return;
         }
-        
+
         AppWidgetManager gm = AppWidgetManager.getInstance(context);
         if (appWidgetIds != null && appWidgetIds.length > 0) {
             gm.updateAppWidget(appWidgetIds, views);
@@ -192,17 +192,17 @@ public class CalendarAppWidgetService extends Service implements Runnable {
 
         // Schedule an alarm to wake ourselves up for the next update.  We also cancel
         // all existing wake-ups because PendingIntents don't match against extras.
-        
+
         // If no next-update calculated, or bad trigger time in past, schedule
         // update about six hours from now.
         if (triggerTime == -1 || triggerTime < now) {
             if (LOGD) Log.w(TAG, "Encountered bad trigger time " + formatDebugTime(triggerTime, now));
             triggerTime = now + UPDATE_NO_EVENTS;
         }
-        
+
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         PendingIntent pendingUpdate = CalendarAppWidgetProvider.getUpdateIntent(context);
-        
+
         am.cancel(pendingUpdate);
         am.set(AlarmManager.RTC, triggerTime, pendingUpdate);
 
@@ -211,7 +211,7 @@ public class CalendarAppWidgetService extends Service implements Runnable {
 
     /**
      * Format given time for debugging output.
-     * 
+     *
      * @param unixTime Target time to report.
      * @param now Current system time from {@link System#currentTimeMillis()}
      *            for calculating time difference.
@@ -219,7 +219,7 @@ public class CalendarAppWidgetService extends Service implements Runnable {
     private String formatDebugTime(long unixTime, long now) {
         Time time = new Time();
         time.set(unixTime);
-        
+
         long delta = unixTime - now;
         if (delta > DateUtils.MINUTE_IN_MILLIS) {
             delta /= DateUtils.MINUTE_IN_MILLIS;
@@ -229,10 +229,10 @@ public class CalendarAppWidgetService extends Service implements Runnable {
             return String.format("[%d] %s (%+d secs)", unixTime, time.format("%H:%M:%S"), delta);
         }
     }
-    
+
     /**
      * Convert given UTC time into current local time.
-     * 
+     *
      * @param recycle Time object to recycle, otherwise null.
      * @param utcTime Time to convert, in UTC.
      */
@@ -249,7 +249,7 @@ public class CalendarAppWidgetService extends Service implements Runnable {
     /**
      * Figure out the next time we should push widget updates, usually the time
      * calculated by {@link #getEventFlip(Cursor, long, long, boolean)}.
-     * 
+     *
      * @param cursor Valid cursor on {@link Instances#CONTENT_URI}
      * @param events {@link MarkedEvents} parsed from the cursor
      */
@@ -260,7 +260,7 @@ public class CalendarAppWidgetService extends Service implements Runnable {
             long start = cursor.getLong(INDEX_BEGIN);
             long end = cursor.getLong(INDEX_END);
             boolean allDay = cursor.getInt(INDEX_ALL_DAY) != 0;
-            
+
             // Adjust all-day times into local timezone
             if (allDay) {
                 final Time recycle = new Time();
@@ -279,7 +279,7 @@ public class CalendarAppWidgetService extends Service implements Runnable {
      * <p>
      * Events with duration longer than one day as treated as all-day events
      * when computing the flipping point.
-     * 
+     *
      * @param start Event start time in local timezone.
      * @param end Event end time in local timezone.
      */
@@ -295,13 +295,13 @@ public class CalendarAppWidgetService extends Service implements Runnable {
     /**
      * Set visibility of various widget components if there are events, or if no
      * events were found.
-     * 
+     *
      * @param views Set of {@link RemoteViews} to apply visibility.
      * @param noEvents True if no events found, otherwise false.
      */
     private void setNoEventsVisible(RemoteViews views, boolean noEvents) {
         views.setViewVisibility(R.id.no_events, noEvents ? View.VISIBLE : View.GONE);
-        
+
         int otherViews = noEvents ? View.GONE : View.VISIBLE;
         views.setViewVisibility(R.id.day_of_month, otherViews);
         views.setViewVisibility(R.id.day_of_week, otherViews);
@@ -309,24 +309,24 @@ public class CalendarAppWidgetService extends Service implements Runnable {
         views.setViewVisibility(R.id.when, otherViews);
         views.setViewVisibility(R.id.title, otherViews);
 
-        // Don't force-show views that are handled elsewhere 
+        // Don't force-show views that are handled elsewhere
         if (noEvents) {
             views.setViewVisibility(R.id.conflict, otherViews);
             views.setViewVisibility(R.id.where, otherViews);
         }
     }
-    
+
     /**
      * Build a set of {@link RemoteViews} that describes how to update any
      * widget for a specific event instance.
-     * 
+     *
      * @param cursor Valid cursor on {@link Instances#CONTENT_URI}
      * @param events {@link MarkedEvents} parsed from the cursor
      */
     private RemoteViews getAppWidgetUpdate(Context context, Cursor cursor, MarkedEvents events) {
         Resources res = context.getResources();
         ContentResolver resolver = context.getContentResolver();
-        
+
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.agenda_appwidget);
         setNoEventsVisible(views, false);
 
@@ -334,17 +334,17 @@ public class CalendarAppWidgetService extends Service implements Runnable {
         time.setToNow();
         int yearDay = time.yearDay;
         int dateNumber = time.monthDay;
-        
+
         // Calendar header
         String dayOfWeek = DateUtils.getDayOfWeekString(time.weekDay + 1,
                 DateUtils.LENGTH_MEDIUM).toUpperCase();
-        
+
         views.setTextViewText(R.id.day_of_week, dayOfWeek);
         views.setTextViewText(R.id.day_of_month, Integer.toString(time.monthDay));
-        
+
         // Fill primary event details
         cursor.moveToPosition(events.primaryRow);
-        
+
         // Color stripe
         /*
         int colorFilter = cursor.getInt(INDEX_COLOR);
@@ -352,11 +352,11 @@ public class CalendarAppWidgetService extends Service implements Runnable {
         views.setTextColor(R.id.title, colorFilter);
         views.setTextColor(R.id.where, colorFilter);
         */
-        
+
         // When
         long start = cursor.getLong(INDEX_BEGIN);
         boolean allDay = cursor.getInt(INDEX_ALL_DAY) != 0;
-        
+
         int flags;
         String whenString;
         if (allDay) {
@@ -364,7 +364,7 @@ public class CalendarAppWidgetService extends Service implements Runnable {
                     | DateUtils.FORMAT_SHOW_DATE;
         } else {
             flags = DateUtils.FORMAT_ABBREV_ALL | DateUtils.FORMAT_SHOW_TIME;
-            
+
             // Show date if different from today
             time.set(start);
             if (yearDay != time.yearDay) {
@@ -387,7 +387,7 @@ public class CalendarAppWidgetService extends Service implements Runnable {
             titleString = context.getString(R.string.no_title_label);
         }
         views.setTextViewText(R.id.title, titleString);
-        
+
         // Conflicts
         int titleLines = 4;
         if (events.primaryCount > 1) {
@@ -400,7 +400,7 @@ public class CalendarAppWidgetService extends Service implements Runnable {
         } else {
             views.setViewVisibility(R.id.conflict, View.GONE);
         }
-        
+
         // Where
         String whereString = cursor.getString(INDEX_EVENT_LOCATION);
         if (whereString != null && whereString.length() > 0) {
@@ -410,11 +410,11 @@ public class CalendarAppWidgetService extends Service implements Runnable {
         } else {
             views.setViewVisibility(R.id.where, View.GONE);
         }
-        
+
         // Trim title lines based on details shown. In landscape we're using
         // singleLine which means this value is ignored.
         views.setInt(R.id.title, "setMaxLines", titleLines);
-        
+
         return views;
     }
 
@@ -436,7 +436,7 @@ public class CalendarAppWidgetService extends Service implements Runnable {
      * Build a {@link PendingIntent} to launch the Calendar app. This correctly
      * sets action, category, and flags so that we don't duplicate tasks when
      * Calendar was also launched from a normal desktop icon.
-     * @param goToTime time that calendar should take the user to 
+     * @param goToTime time that calendar should take the user to
      */
     private PendingIntent getLaunchPendingIntent(Context context, long goToTime) {
         Intent launchIntent = new Intent();
@@ -468,7 +468,7 @@ public class CalendarAppWidgetService extends Service implements Runnable {
      * Walk the given instances cursor and build a list of marked events to be
      * used when updating the widget. This structure is also used to check if
      * updates are needed.
-     * 
+     *
      * @param cursor Valid cursor across {@link Instances#CONTENT_URI}.
      * @param watchEventIds Specific events to watch for, setting
      *            {@link MarkedEvents#watchFound} if found during marking.
@@ -478,7 +478,7 @@ public class CalendarAppWidgetService extends Service implements Runnable {
     private MarkedEvents buildMarkedEvents(Cursor cursor, Set<Long> watchEventIds, long now) {
         MarkedEvents events = new MarkedEvents();
         final Time recycle = new Time();
-        
+
         cursor.moveToPosition(-1);
         while (cursor.moveToNext()) {
             int row = cursor.getPosition();
@@ -492,19 +492,19 @@ public class CalendarAppWidgetService extends Service implements Runnable {
                 start = convertUtcToLocal(recycle, start);
                 end = convertUtcToLocal(recycle, end);
             }
-            
+
             // Skip events that have already passed their flip times
             long eventFlip = getEventFlip(cursor, start, end, allDay);
             if (LOGD) Log.d(TAG, "Calculated flip time " + formatDebugTime(eventFlip, now));
             if (eventFlip < now) {
                 continue;
             }
-            
+
             // Mark if we've encountered the watched event
             if (watchEventIds.contains(eventId)) {
                 events.watchFound = true;
             }
-            
+
             if (events.primaryRow == -1) {
                 // Found first event
                 events.primaryRow = row;
@@ -535,7 +535,7 @@ public class CalendarAppWidgetService extends Service implements Runnable {
     /**
      * Query across all calendars for upcoming event instances from now until
      * some time in the future.
-     * 
+     *
      * @param resolver {@link ContentResolver} to use when querying
      *            {@link Instances#CONTENT_URI}.
      * @param searchDuration Distance into the future to look for event
@@ -547,14 +547,14 @@ public class CalendarAppWidgetService extends Service implements Runnable {
             long searchDuration, long now) {
         // Search for events from now until some time in the future
         long end = now + searchDuration;
-        
+
         Uri uri = Uri.withAppendedPath(Instances.CONTENT_URI,
                 String.format("%d/%d", now, end));
 
         String selection = String.format("%s=1 AND %s!=%d",
                 Calendars.SELECTED, Instances.SELF_ATTENDEE_STATUS,
                 Attendees.ATTENDEE_STATUS_DECLINED);
-        
+
         return resolver.query(uri, EVENT_PROJECTION, selection, null,
                 EVENT_SORT_ORDER);
     }

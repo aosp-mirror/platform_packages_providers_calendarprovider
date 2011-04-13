38135d41c14217f19736f060d40429d5b8a43f8a
RoboErik <epastern@google.com>
2011-04-13 12:55:10 -0700

    Add test for adding/deleting an event
    Also refactored to make reuse across tests easier.
    
    Change-Id: I7970ce446a464df7612350d1876d8edb9a4f4c8d

--- tests/src/com/android/providers/calendar/CalendarCts.java
+++ tests/src/com/android/providers/calendar/CalendarCts.java
@@ -24,23 +24,29 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.provider.Calendar;
 import android.provider.Calendar.Calendars;
+import android.provider.Calendar.Events;
 import android.test.InstrumentationTestCase;
 import android.test.suitebuilder.annotation.*;
+import android.text.format.DateUtils;
 import android.util.Log;
 
 public class CalendarCts extends InstrumentationTestCase {
 
-
-    /**
-     *
-     */
     private static final String TAG = "CalCTS";
+    private static final String CTS_TEST_TYPE = "LOCAL";
+    // @formatter:off
+    private static final String[] TIME_ZONES = new String[] {
+            "UTC",
+            "America/Los_Angeles",
+            "Asia/Beirut",
+            "Pacific/Auckland", };
+    // @formatter:on
+
     private ContentResolver mContentResolver;
 
     private static class CalendarHelper {
-        private static final String CTS_TEST_TYPE = "LOCAL";
-        private static final String[] TIME_ZONES = new String[] {
-                "UTC", "America/Los_Angeles", "Asia/Beirut", "Pacific/Auckland", };
+
+        // @formatter:off
         public static final String[] CALENDARS_PROJECTION = new String[] {
                 Calendars._ID,
                 Calendars._SYNC_ACCOUNT,
@@ -60,12 +66,16 @@ public class CalendarCts extends InstrumentationTestCase {
                 Calendars.TIMEZONE,
                 Calendars.OWNER_ACCOUNT,
                 Calendars.ORGANIZER_CAN_RESPOND,
+                // Calendars.CAN_CHANGE_TZ,
+                // Calendars.MAX_REMINDERS,
                 Calendars.DELETED,
                 Calendars.SYNC1,
                 Calendars.SYNC2,
                 Calendars.SYNC3,
                 Calendars.SYNC4,
-                Calendars.SYNC5, };
+                Calendars.SYNC5,
+                };
+        // @formatter:on
 
         /**
          * Creates a new set of values for creating a single calendar with every
@@ -156,6 +166,157 @@ public class CalendarCts extends InstrumentationTestCase {
         }
     }
 
+    // @formatter:off
+    private static class EventHelper {
+        public static final String[] EVENTS_PROJECTION = new String[] {
+            Events._ID,
+            Events._SYNC_ACCOUNT,
+            Events._SYNC_ACCOUNT_TYPE,
+            Events.OWNER_ACCOUNT,
+            // Events.ORGANIZER_CAN_RESPOND, from Calendars
+            // Events.CAN_CHANGE_TZ, from Calendars
+            // Events.MAX_REMINDERS, from Calendars
+            Events.CALENDAR_ID,
+            // Events.CALENDAR_DISPLAY_NAME, from Calendars
+            // Events.CALENDAR_COLOR, from Calendars
+            // Events.CALENDAR_ACL, from Calendars
+            // Events.CALENDAR_VISIBLE, from Calendars
+            Events.HTML_URI,
+            Events.COMMENTS_URI,
+            Events.TITLE,
+            Events.EVENT_LOCATION,
+            Events.DESCRIPTION,
+            Events.STATUS,
+            Events.SELF_ATTENDEE_STATUS,
+            Events.DTSTART,
+            Events.DTEND,
+            Events.EVENT_TIMEZONE,
+            // Events.EVENT_TIMEZONE2,
+            Events.DURATION,
+            Events.ALL_DAY,
+            Events.VISIBILITY,
+            Events.TRANSPARENCY,
+            Events.HAS_ALARM,
+            // Events.ALARM_MINUTES
+            Events.HAS_EXTENDED_PROPERTIES,
+            Events.RRULE,
+            Events.RDATE,
+            Events.EXRULE,
+            Events.EXDATE,
+            // Events.ORIGINAL_ID
+            Events.ORIGINAL_EVENT, // rename ORIGINAL_SYNC_ID
+            Events.ORIGINAL_INSTANCE_TIME,
+            Events.ORIGINAL_ALL_DAY,
+            Events.LAST_DATE,
+            Events.HAS_ATTENDEE_DATA,
+            Events.GUESTS_CAN_MODIFY,
+            Events.GUESTS_CAN_INVITE_OTHERS,
+            Events.GUESTS_CAN_SEE_GUESTS,
+            Events.ORGANIZER,
+            Events.DELETED,
+            Events._SYNC_ID,
+            Events._SYNC_VERSION,
+            Events._SYNC_TIME,
+            Events._SYNC_DIRTY,
+            // Events._SYNC_MARK,
+            Events._SYNC_DATA, // Events.SYNC_DATA1
+            // Events.SYNC_DATA2
+            // Events.SYNC_DATA3
+            // Events.SYNC_DATA4
+        };
+        // @formatter:on
+
+        public static ContentValues getNewEventValues(
+                String account, int seed, long calendarId) {
+            String seedString = Long.toString(seed);
+            ContentValues values = new ContentValues();
+            values.put(Events._SYNC_ID, "SYNC_ID:" + seedString);
+            values.put(Events._SYNC_VERSION, "SYNC_V:" + seedString);
+            values.put(Events._SYNC_TIME, "SYNC_TIME:" + seedString);
+            // values.put(Events._SYNC_DIRTY, 0);
+            // values.put(Events._SYNC_MARK, 0);
+            values.put(Events.ORGANIZER, "ORGANIZER:" + seedString);
+
+            values.put(Events.TITLE, seedString);
+            values.put(Events.EVENT_LOCATION, "LOCATION_" + seedString);
+
+            values.put(Events.CALENDAR_ID, calendarId);
+
+            values.put(Events.HTML_URI, "HTML:" + seedString);
+            values.put(Events.COMMENTS_URI, "COMMENTS:" + seedString);
+            values.put(Events.DESCRIPTION, "DESCRIPTION:" + seedString);
+            values.put(Events.STATUS, seed % 3);
+
+            values.put(Events.DTSTART, seed);
+            values.put(Events.DTEND, seed + DateUtils.HOUR_IN_MILLIS);
+            values.put(Events.EVENT_TIMEZONE, TIME_ZONES[seed % TIME_ZONES.length]);
+            // values.put(Events.EVENT_TIMEZONE2, TIME_ZONES[(seed +1) %
+            // TIME_ZONES.length]);
+            values.put(Events.ALL_DAY, 0); // must be 0 or dtstart/dtend will
+                                           // get adjusted
+            values.put(Events.VISIBILITY, seed % 4);
+            values.put(Events.TRANSPARENCY, seed % 2);
+            values.put(Events.HAS_ALARM, seed % 2);
+            values.put(Events.HAS_EXTENDED_PROPERTIES, seed % 2);
+            values.put(Events.HAS_ATTENDEE_DATA, seed % 2);
+            values.put(Events.GUESTS_CAN_MODIFY, seed % 2);
+            values.put(Events.GUESTS_CAN_INVITE_OTHERS, seed % 2);
+            values.put(Events.GUESTS_CAN_SEE_GUESTS, seed % 2);
+
+            // values.put(Events.SYNC1, "SYNC1:" + seedString);
+            // values.put(Events.SYNC2, "SYNC2:" + seedString);
+            // values.put(Events.SYNC3, "SYNC3:" + seedString);
+            // values.put(Events.SYNC4, "SYNC4:" + seedString);
+            // values.put(Events.SYNC5, "SYNC5:" + seedString);
+//            Events.RRULE,
+//            Events.RDATE,
+//            Events.EXRULE,
+//            Events.EXDATE,
+//            // Events.ORIGINAL_ID
+//            Events.ORIGINAL_EVENT, // rename ORIGINAL_SYNC_ID
+//            Events.ORIGINAL_INSTANCE_TIME,
+//            Events.ORIGINAL_ALL_DAY,
+
+            return values;
+        }
+
+        public static void addDefaultReadOnlyValues(ContentValues values, String account) {
+            values.put(Events.SELF_ATTENDEE_STATUS, Events.STATUS_TENTATIVE);
+            values.put(Events.DELETED, 0);
+            values.put(Events._SYNC_DIRTY, 1);
+            values.put(Events.OWNER_ACCOUNT, "OWNER_" + account);
+            values.put(Events._SYNC_ACCOUNT_TYPE, CTS_TEST_TYPE);
+            values.put(Events._SYNC_ACCOUNT, account);
+        }
+
+        public static void deleteEvent(ContentResolver resolver, Uri uri, ContentValues values) {
+            resolver.delete(uri, null, null);
+            values.put(Events.DELETED, 1);
+        }
+
+        public static void deleteEventAsSyncAdapter(ContentResolver resolver, Uri uri,
+                ContentValues values) {
+            Uri syncUri = uri.buildUpon()
+                    .appendQueryParameter(Calendar.CALLER_IS_SYNCADAPTER, "true").build();
+            resolver.delete(syncUri, null, null);
+        }
+
+        public static Cursor getEventsByAccount(ContentResolver resolver, String account) {
+            String selection = Calendars._SYNC_ACCOUNT_TYPE + "=?";
+            String[] selectionArgs;
+            if (account != null) {
+                selection += " AND " + Calendars._SYNC_ACCOUNT + "=?";
+                selectionArgs = new String[2];
+                selectionArgs[1] = account;
+            } else {
+                selectionArgs = new String[1];
+            }
+            selectionArgs[0] = CTS_TEST_TYPE;
+            return resolver.query(Events.CONTENT_URI, EVENTS_PROJECTION, selection, selectionArgs,
+                    null);
+        }
+    }
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
@@ -171,23 +332,9 @@ public class CalendarCts extends InstrumentationTestCase {
 
         // Clean up just in case
         CalendarHelper.deleteCalendarByAccount(mContentResolver, account);
+        long id = createAndVerifyCalendar(account, seed++, null);
 
-        // Create a calendar
-        ContentValues values = CalendarHelper.getNewCalendarValues(account, seed++);
-        Uri uri = mContentResolver.insert(Calendars.CONTENT_URI, values);
-        Log.d(TAG, "uri:" + uri.toString());
-        long id = ContentUris.parseId(uri);
-        assertTrue(id >= 0);
-
-        verifyCalendar(account, values, id);
-
-        // Delete
-        assertEquals(1, CalendarHelper.deleteCalendarById(mContentResolver, id));
-
-        // Verify
-        Cursor c = CalendarHelper.getCalendarsByAccount(mContentResolver, account);
-        assertEquals(0, c.getCount());
-        c.close();
+        deleteAndVerifyCalendar(account, id);
     }
 
     @MediumTest
@@ -199,11 +346,10 @@ public class CalendarCts extends InstrumentationTestCase {
         CalendarHelper.deleteCalendarByAccount(mContentResolver, account);
 
         // Create a calendar
-        ContentValues values = CalendarHelper.getNewCalendarValues(account, seed++);
-        Uri uri = mContentResolver.insert(Calendars.CONTENT_URI, values);
-        Log.d(TAG, "uri:" + uri.toString());
-        long id = ContentUris.parseId(uri);
-        assertTrue(id >= 0);
+        ContentValues values = CalendarHelper.getNewCalendarValues(account, seed);
+        long id = createAndVerifyCalendar(account, seed++, values);
+
+        Uri uri = ContentUris.withAppendedId(Calendars.CONTENT_URI, id);
 
         // Update the calendar using the direct Uri
         ContentValues updateValues = CalendarHelper.getUpdateCalendarValuesWithOriginal(
@@ -215,7 +361,6 @@ public class CalendarCts extends InstrumentationTestCase {
         // Update the calendar using selection + args
         String selection = Calendars._ID + "=?";
         String[] selectionArgs = new String[] { Long.toString(id) };
-        Log.d(TAG, "args:" + selectionArgs.toString());
 
         updateValues = CalendarHelper.getUpdateCalendarValuesWithOriginal(values, seed++);
 
@@ -224,6 +369,108 @@ public class CalendarCts extends InstrumentationTestCase {
 
         verifyCalendar(account, values, id);
 
+        deleteAndVerifyCalendar(account, id);
+    }
+
+    private void verifyEvent(ContentValues values, long eventId) {
+        Uri eventUri = ContentUris.withAppendedId(Events.CONTENT_URI, eventId);
+        // Verify
+        Cursor c = mContentResolver
+                .query(eventUri, EventHelper.EVENTS_PROJECTION, null, null, null);
+        assertEquals(1, c.getCount());
+        assertTrue(c.moveToFirst());
+        assertEquals(eventId, c.getLong(0));
+        for (String key : values.keySet()) {
+            int index = c.getColumnIndex(key);
+            assertEquals(key, values.getAsString(key), c.getString(index));
+        }
+        c.close();
+    }
+
+    @MediumTest
+    public void testEventCreationAndDeletion() {
+        String account = "ec1_account";
+        int seed = 0;
+
+        // Clean up just in case
+        CalendarHelper.deleteCalendarByAccount(mContentResolver, account);
+
+        // Create calendar and event
+        long calendarId = createAndVerifyCalendar(account, seed++, null);
+
+        ContentValues eventValues = EventHelper.getNewEventValues(account, seed++, calendarId);
+        long eventId = createAndVerifyEvent(account, seed, calendarId, eventValues);
+
+        Uri eventUri = ContentUris.withAppendedId(Events.CONTENT_URI, eventId);
+
+        // Delete event
+        EventHelper.deleteEvent(mContentResolver, eventUri, eventValues);
+        // Verify
+        verifyEvent(eventValues, eventId);
+        // Delete as sync adapter
+        EventHelper.deleteEventAsSyncAdapter(mContentResolver, eventUri, eventValues);
+        // Verify
+        Cursor c = EventHelper.getEventsByAccount(mContentResolver, account);
+        assertEquals(0, c.getCount());
+        c.close();
+
+        deleteAndVerifyCalendar(account, calendarId);
+    }
+
+    /**
+     * Creates an event on the given calendar and verifies it.
+     * 
+     * @param account
+     * @param seed
+     * @param calendarId
+     * @param values optional pre created set of values
+     * @return the _id for the new event
+     */
+    private long createAndVerifyEvent(String account, int seed, long calendarId,
+            ContentValues values) {
+        // Create an event
+        if (values == null) {
+            values = EventHelper.getNewEventValues(account, seed, calendarId);
+        }
+        Uri uri = mContentResolver.insert(Events.CONTENT_URI, values);
+
+        // Verify
+        EventHelper.addDefaultReadOnlyValues(values, account);
+        long eventId = ContentUris.parseId(uri);
+        assertTrue(eventId >= 0);
+
+        verifyEvent(values, eventId);
+        return eventId;
+    }
+
+    /**
+     * Inserts a new calendar with the given account and seed and verifies it.
+     *
+     * @param account The account to add the calendar to
+     * @param seed A number to use to generate the values
+     * @return the created calendar's id
+     */
+    private long createAndVerifyCalendar(String account, int seed, ContentValues values) {
+        // Create a calendar
+        if (values == null) {
+            values = CalendarHelper.getNewCalendarValues(account, seed);
+        }
+        Uri uri = mContentResolver.insert(Calendars.CONTENT_URI, values);
+        long calendarId = ContentUris.parseId(uri);
+        assertTrue(calendarId >= 0);
+
+        verifyCalendar(account, values, calendarId);
+        return calendarId;
+    }
+
+    /**
+     * Deletes a given calendar and verifies no calendars remain on that
+     * account.
+     *
+     * @param account
+     * @param id
+     */
+    private void deleteAndVerifyCalendar(String account, long id) {
         // Delete
         assertEquals(1, CalendarHelper.deleteCalendarById(mContentResolver, id));
 
@@ -234,9 +481,12 @@ public class CalendarCts extends InstrumentationTestCase {
     }
 
     /**
-     * @param account
-     * @param values
-     * @param id
+     * Check all the fields of a calendar contained in values + id. This assumes
+     * a single calendar has been created on the given account.
+     *
+     * @param account the account of the calendar
+     * @param values the values to check against the db
+     * @param id the _id of the calendar
      */
     private void verifyCalendar(String account, ContentValues values, long id) {
         // Verify
@@ -246,7 +496,7 @@ public class CalendarCts extends InstrumentationTestCase {
         assertEquals(id, c.getLong(0));
         for (String key : values.keySet()) {
             int index = c.getColumnIndex(key);
-            assertEquals(values.getAsString(key), c.getString(index));
+            assertEquals(key, values.getAsString(key), c.getString(index));
         }
         c.close();
     }

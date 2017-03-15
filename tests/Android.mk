d8a5aaf77ce9fc423b6fa118b4238630e39b6590
Simran Basi <sbasi@google.com>
2017-03-15 15:14:31 -0700

    packages/providers/CalendarProvider: Add APCT tests to the device-tests suite.
    This CL adds the APCT tests within this project to
    a similar suite as CTS known as device-tests.
    
    The current method of running APCT tests in the infrastructure
    is unaffected.
    
    Bug: 35882476
    Test: `make dist device-tests -j` and local builds of
          continuous_instrumentation_tests & continuous_native_tests
    
    Change-Id: Idb501205311f6c82490cd2374ed5a4037aaffe49

--- tests/Android.mk
+++ tests/Android.mk
@@ -8,6 +8,7 @@ LOCAL_MODULE_TAGS := tests
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := CalendarProviderTests
+LOCAL_COMPATIBILITY_SUITE := device-tests
 
 LOCAL_STATIC_JAVA_LIBRARIES := calendar-common junit legacy-android-test
 
